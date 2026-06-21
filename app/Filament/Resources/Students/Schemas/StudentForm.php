1de2b56a59debdddf9a0b9d4cfd95018f8a71b8e
Shakira Arunika <shakirarunika@gmail.com>
2026-06-21 21:31:45 +0700

    fix: perbaiki namespace Section untuk Filament v5

--- app/Filament/Resources/Students/Schemas/StudentForm.php
+++ app/Filament/Resources/Students/Schemas/StudentForm.php
@@ -6,10 +6,10 @@ use App\Models\AcademicYear;
 use App\Models\Classroom;
 use App\Models\Student;
 use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class StudentForm
